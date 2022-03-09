20e3e2b6adf9b9158be234242f79417e0826e958
Mattia Cerrato <mcerrato@uni-mainz.de>
2022-03-09 11:14:53 +0100

    run.py: SubprocEnv

--- run.py
+++ run.py
@@ -1,15 +1,54 @@
 import os
 from stable_baselines3 import SAC
-from stable_baselines3.sac.policies import SACPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.env_util import make_vec_env
 import argparse
-from utils import options, check_args, constant_schedule
 import gym
+import datetime
 import pybulletgym
 import pybullet_envs
 import wandb
 from wandb.integration.sb3 import WandbCallback
+
+options = {
+    "FOLLOW_STEPS" : 1,
+    "MIX_AGENTS" : 0,
+    "SWITCH_TRAIN" : 1,
+    "SWITCH_RATIO" : 1,
+    "USE_PRIO" : 0,
+    "SAVE_NAME" : "temp",
+    "LOAD" : 0,
+    "HIDDEN_SIZE" : 256,
+    "AGENT_COUNT" : 4,
+    "PEER_LEARNING" : 1,
+    "USE_AGENT_VALUE" : 0,
+    "USE_TRUST" : 1,
+    "USE_TRUST_BUFFER" : 1,
+    "USE_CRITIC" : 1,
+    "T" : 1,
+    "T_DECAY" : 0,
+    "TRUST_LR" : 0.001,
+    "RENDER_TRAIN" : 0,
+    "GPU": -1,
+    "STEPS": 3_000_000,#200*100,#
+    "EVAL_INTERVAL" : 1,
+    "EVAL_N_RUNS" : 10,
+    "REPLAY_START_SIZE" : 10000,#,200,#
+    "RBUF_CAPACITY" : 10**6,
+    "TEST": False,
+    "ENV" : "HalfCheetahPyBulletEnv-v0",#Pendulum-v0"#""Pendulum-v0"#"Walker2DPyBulletEnv-v0" #"InvertedDoublePendulumPyBulletEnv-v0"#"BipedalWalker-v3"#
+    "N_AGENTS": 4,
+    "BASESAVELOC": "./agents/",
+    "SAVEDIR": datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'),
+    "LOGINTERVAL": 1000
+}
+
+
+def check_args(args):
+    assert(args.peer_learning if args.use_trust else True)
+    assert(args.peer_learning if args.use_critic else True)
+    assert(args.peer_learning if args.use_agent_value else True)
 
 def add_args():
     parser = argparse.ArgumentParser()
@@ -115,7 +154,8 @@ if __name__ == '__main__':
     parser = add_args()
     args = parser.parse_args()
     wandb.init(entity='jgu-wandb', config=args, project='peer-learning', monitor_gym=True)
-    train_env = make_env(args.env, args.test)
+    #train_env = SubprocVecEnv([make_env(args.env, args.test) for i in range(args.num_agents)])
+    train_env = make_vec_env(args.env, n_envs=args.num_agents, seed=0)
     test_env = make_env(args.env, True)
     check_args(args)
     agents = create_sac_agents(train_env, args.num_agents)
