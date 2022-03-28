20adea8f9c04271359035a37fe3ae82e2eb7f67c
vanillawhey <cedric.derstroff@gmx.net>
2022-03-28 18:35:34 +0200

    Improved the suggestion buffer; Could, however, be replaced by a direct use of the deque.

--- suggestionbuffer.py
+++ suggestionbuffer.py
@@ -1,25 +1,22 @@
+import random
 import numpy as np
+from collections import deque
 
 
 class SuggestionBuffer:
     def __init__(self, capacity):
-        self.buffer = []
-        self.size = 0
-        self.index = 0
-        self.capacity = capacity
+        self.buffer = deque(maxlen=capacity)
 
-    def add(self, reward, suggested_action, followed_agent, next_obs):
-        data = [reward, suggested_action, followed_agent, next_obs]
-        if (self.size == self.capacity):
-            self.buffer[self.index] = data
-            self.index = (self.index + 1) % self.capacity
-        else:
-            self.buffer.append(data)
-            self.size += 1
+    def add(self, *args):
+        self.buffer.append(args)
 
     def sample(self, batch_size):
-        indices = np.random.choice(self.size, batch_size)
-        return [self.buffer[index] for index in indices]
+        if len(self.buffer) > batch_size:
+            return random.sample(self.buffer, batch_size)
+        # else return None
+
+    def latest(self):
+        return self.buffer[-1]
 
 
 if __name__ == "__main__":
