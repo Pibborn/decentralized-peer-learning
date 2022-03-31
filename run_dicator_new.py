11069de3fdda933c56fcba512aa8850ad9b4a626
vanillawhey <cedric.derstroff@gmx.net>
2022-03-31 14:43:16 +0200

    Refactoring: Reordered default parameters, grouped parser args

--- run_dicator_new.py
+++ run_dicator_new.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+
 import gym
 import pybulletgym  # noqa
 import pybullet_envs  # noqa
@@ -6,8 +8,9 @@ import pybullet_envs  # noqa
 from pathlib import Path
 
 from stable_baselines3 import SAC, TD3
-from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.env_util import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 
 import wandb
 from wandb.integration.sb3 import WandbCallback
@@ -18,79 +21,97 @@ from new_dictator import make_dictator_class
 
 # default options for the argument parser
 options = {
-    "MIX_AGENTS": False,
-    "SAVE_NAME": "Dictator",
-    "LOAD": False,
-    "NET_ARCH": [400, 300],
+    # General
+    "SAVE_NAME": "dictator",
     "AGENT_COUNT": 4,
-    "T": 1,
-    "T_DECAY": 0,
     "DEVICE": "auto",
+    "ENV": "HalfCheetahBulletEnv-v0",
+    # Training
     "STEPS": 3_000_000,
     "EVAL_INTERVAL": 10_000,
     "EVAL_N_RUNS": 10,
     "BUFFER_START_SIZE": 1_000,
     "BUFFER_SIZE": 1_000_000,
-    "ENV": "HalfCheetahBulletEnv-v0",
     "BATCH_SIZE": 100,
     "MIN_EPOCH_LEN": 10_000,
+    "LEARNING_RATE": 3e-4,
+    "TAU": 0.005,
+    "GAMMA": 0.99,
+    "GRAD_STEPS": 1,
+    "TRAIN_FREQ": 1,
+    # Agents
+    "MIX_AGENTS": False,
+    "NET_ARCH": [400, 300],
+    # Dictator
+    "T": 1,
+    "T_DECAY": 0,
+    # WANDB
     "WANDB": "offline",
 }
 
 # create arg parser
-parser = argparse.ArgumentParser()
-parser.add_argument("--net-arch", type=list,
-                    default=options["NET_ARCH"])
-parser.add_argument("--mix-agents", type=bool,
-                    default=options["MIX_AGENTS"])
-parser.add_argument("--buffer-size", type=int,
-                    default=options["BUFFER_SIZE"])
-parser.add_argument("--agent-count", type=int,
-                    default=options["AGENT_COUNT"])
-parser.add_argument("--sample_actions", action="store_true")
-parser.add_argument("--greedy_suggestions", action="store_true")
-parser.add_argument("--steps", type=int, default=options["STEPS"],
-                    help="Total number of time steps to train the agent.")
-parser.add_argument("--eval-interval", type=int,
-                    default=options["EVAL_INTERVAL"],
-                    help="Interval in time steps between evaluations.")
-parser.add_argument("--n-eval-episodes", type=int,
-                    default=options["EVAL_N_RUNS"],
-                    help="Number of episodes for each evaluation.")
+parser = argparse.ArgumentParser(description="Dictator.")
+# General
 parser.add_argument("--save-name", type=str, default=options["SAVE_NAME"])
-parser.add_argument("--T", type=float, default=options["T"])
-parser.add_argument("--T-decay", type=float, default=options["T_DECAY"])
-parser.add_argument("--learning_rate", type=float, default=3e-4)
-parser.add_argument("--tau", type=float, default=0.005)
-parser.add_argument("--gamma", type=float, default=0.99)
-parser.add_argument("--gradient_steps", type=int, default=1)
-parser.add_argument("--train_freq", type=int, default=1)
-parser.add_argument("--env", type=str, default=options["ENV"],
-                    help="OpenAI Gym environment to perform algorithm on.")
+parser.add_argument("--agent-count", type=int, help="Number of agents.",
+                    default=options["AGENT_COUNT"])
 parser.add_argument("--device", type=str, default=options["DEVICE"],
                     choices=["cpu", "cuda", "auto"],
                     help="Device to use, either 'cpu', 'cuda' for GPU or "
                          "'auto'.")
-parser.add_argument("--load", type=bool, default=options["LOAD"])
+parser.add_argument("--env", type=str, default=options["ENV"],
+                    help="OpenAI Gym environment to perform algorithm on.")
 parser.add_argument("--seed", type=int, default=1,
                     help="Random seed in [0, 2 ** 32)")
-parser.add_argument("--buffer-start-size", type=int,
-                    default=options["BUFFER_START_SIZE"],
-                    help="Minimum replay buffer size before performing "
-                         "gradient updates.")
-parser.add_argument("--batch-size", type=int, default=options["BATCH_SIZE"],
-                    help="Minibatch size")
-parser.add_argument("--min-epoch-length", type=int,
-                    default=options["MIN_EPOCH_LEN"],
-                    help="Minimal length of a training epoch.")
-parser.add_argument("--wandb", type=str, default=options["WANDB"])
+# Training
+training = parser.add_argument_group("Training")
+training.add_argument("--steps", type=int, default=options["STEPS"],
+                      help="Total number of time steps to train the agent.")
+training.add_argument("--eval-interval", type=int,
+                      default=options["EVAL_INTERVAL"],
+                      help="Interval in time steps between evaluations.")
+training.add_argument("--n-eval-episodes", type=int,
+                      default=options["EVAL_N_RUNS"],
+                      help="Number of episodes for each evaluation.")
+training.add_argument("--buffer-size", type=int,
+                      default=options["BUFFER_SIZE"])
+training.add_argument("--buffer-start-size", type=int,
+                      default=options["BUFFER_START_SIZE"],
+                      help="Minimum replay buffer size before performing "
+                           "gradient updates.")
+training.add_argument("--batch-size", type=int, default=options["BATCH_SIZE"],
+                      help="Minibatch size")
+training.add_argument("--min-epoch-length", type=int,
+                      default=options["MIN_EPOCH_LEN"],
+                      help="Minimal length of a training epoch.")
+training.add_argument("--learning_rate", type=float,
+                      default=options["LEARNING_RATE"])
+training.add_argument("--tau", type=float, default=options["TAU"])
+training.add_argument("--gamma", type=float, default=options["GAMMA"])
+training.add_argument("--gradient_steps", type=int,
+                      default=options["GRAD_STEPS"])
+training.add_argument("--train_freq", type=int, default=options["TRAIN_FREQ"])
+# Agents
+agent_parser = parser.add_argument_group("Agent")
+agent_parser.add_argument("--mix-agents", type=bool,
+                          default=options["MIX_AGENTS"])
+agent_parser.add_argument("--net-arch", type=list,
+                          default=options["NET_ARCH"])
+# Dictator
+dictator = parser.add_argument_group("Dictator")
+dictator.add_argument("--T", type=float, default=options["T"])
+dictator.add_argument("--T-decay", type=float, default=options["T_DECAY"])
+# WANDB
+parser.add_argument("--wandb", type=str, default=options["WANDB"],
+                    choices=["online", "offline", "disabled"])
 
 # parse args
 args = parser.parse_args()
 
 # create results/experiments folder
+unique_dir = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 experiment_folder = Path.cwd().joinpath("Experiments", args.save_name,
-                                        wandb.util.generate_id())
+                                        unique_dir)
 experiment_folder.mkdir(exist_ok=True, parents=True)
 
 # init wandb
@@ -108,7 +129,7 @@ def make_env(seed=0):
     env = gym.make(args.env)
     env.seed(seed)
     env = Monitor(env)
-    return env
+    return DummyVecEnv([lambda: env])
 
 
 # initialize dictator
@@ -116,9 +137,11 @@ algo_args = dict(policy="MlpPolicy", verbose=1,
                  policy_kwargs=dict(log_std_init=-3, net_arch=args.net_arch),
                  buffer_size=args.buffer_size,
                  batch_size=args.batch_size,
-                 ent_coef='auto', gamma=args.gamma, tau=args.tau, train_freq=args.train_freq,
+                 ent_coef='auto', gamma=args.gamma, tau=args.tau,
+                 train_freq=args.train_freq,
+                 gradient_steps=args.gradient_steps,
                  learning_starts=args.buffer_start_size, use_sde=True,
-                 learning_rate=args.learning_rate, gradient_steps=args.gradient_steps,
+                 learning_rate=args.learning_rate,
                  tensorboard_log=str(experiment_folder),
                  device=args.device)
 
--- run_peer.py
+++ run_peer.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+
 import gym
 import pybulletgym  # noqa
 import pybullet_envs  # noqa
@@ -6,8 +8,9 @@ import pybullet_envs  # noqa
 from pathlib import Path
 
 from stable_baselines3 import SAC, TD3
-from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.env_util import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 
 import wandb
 from wandb.integration.sb3 import WandbCallback
@@ -16,13 +19,30 @@ from peer import PeerGroup, make_peer_class
 
 # default options for the argument parser
 options = {
-    "FOLLOW_STEPS": 1,
-    "MIX_AGENTS": False,
-    "SWITCH_RATIO": 1,
-    "SAVE_NAME": "Peers",
-    "LOAD": False,
-    "NET_ARCH": [400, 300],
+    # General
+    "SAVE_NAME": "peer_learning",
     "AGENT_COUNT": 4,
+    "DEVICE": "auto",
+    "ENV": "HalfCheetahBulletEnv-v0",
+    # Training
+    "STEPS": 3_000_000,
+    "EVAL_INTERVAL": 10_000,
+    "EVAL_N_RUNS": 10,
+    "BUFFER_START_SIZE": 1_000,
+    "BUFFER_SIZE": 1_000_000,
+    "BATCH_SIZE": 100,
+    "MIN_EPOCH_LEN": 10_000,
+    "LEARNING_RATE": 3e-4,
+    "TAU": 0.005,
+    "GAMMA": 0.99,
+    "GRAD_STEPS": 1,
+    "TRAIN_FREQ": 1,
+    # Agents
+    "MIX_AGENTS": False,
+    "NET_ARCH": [400, 300],
+    # Peer Learning
+    "FOLLOW_STEPS": 1,
+    "SWITCH_RATIO": 1,
     "PEER_LEARNING": True,
     "USE_AGENT_VALUE": True,
     "USE_TRUST": True,
@@ -31,79 +51,83 @@ options = {
     "T": 1,
     "T_DECAY": 0,
     "TRUST_LR": 0.001,
-    "DEVICE": "auto",
-    "STEPS": 3_000_000,
-    "EVAL_INTERVAL": 10_000,
-    "EVAL_N_RUNS": 10,
-    "BUFFER_START_SIZE": 1_000,
-    "BUFFER_SIZE": 1_000_000,
-    "ENV": "HalfCheetahBulletEnv-v0",
-    "BATCH_SIZE": 100,
-    "MIN_EPOCH_LEN": 10_000,
+    # WANDB
     "WANDB": "offline",
 }
 
 # create arg parser
-parser = argparse.ArgumentParser()
-parser.add_argument("--follow-steps", type=int,
-                    default=options["FOLLOW_STEPS"])
-parser.add_argument("--net-arch", type=list,
-                    default=options["NET_ARCH"])
-parser.add_argument("--mix-agents", type=bool,
-                    default=options["MIX_AGENTS"])
-parser.add_argument("--switch-ratio", type=float,
-                    default=options["SWITCH_RATIO"],
-                    help="Ratio of peer learning episodes to solo episodes; "
-                         "0 means only peer learning episodes.")
-parser.add_argument("--buffer-size", type=int,
-                    default=options["BUFFER_SIZE"])
-parser.add_argument("--agent-count", type=int,
-                    default=options["AGENT_COUNT"])
-parser.add_argument("--use-agent-value", type=bool,
-                    default=options["USE_AGENT_VALUE"])
-parser.add_argument("--use-trust", type=bool, default=options["USE_TRUST"])
-parser.add_argument("--use-trust-buffer", type=bool,
-                    default=options["USE_TRUST_BUFFER"])
-parser.add_argument("--use-critic", type=bool,
-                    default=options["USE_CRITIC"])
-parser.add_argument("--peer-learning", type=bool,
-                    default=options["PEER_LEARNING"])
-parser.add_argument("--steps", type=int, default=options["STEPS"],
-                    help="Total number of time steps to train the agent.")
-parser.add_argument("--eval-interval", type=int,
-                    default=options["EVAL_INTERVAL"],
-                    help="Interval in time steps between evaluations.")
-parser.add_argument("--n-eval-episodes", type=int,
-                    default=options["EVAL_N_RUNS"],
-                    help="Number of episodes for each evaluation.")
-parser.add_argument("--trust-lr", type=float, default=options["TRUST_LR"])
+parser = argparse.ArgumentParser(description="Peer learning.")
+# General
 parser.add_argument("--save-name", type=str, default=options["SAVE_NAME"])
-parser.add_argument("--T", type=float, default=options["T"])
-parser.add_argument("--T-decay", type=float, default=options["T_DECAY"])
-parser.add_argument("--learning_rate", type=float, default=3e-4)
-parser.add_argument("--tau", type=float, default=0.005)
-parser.add_argument("--gamma", type=float, default=0.99)
-parser.add_argument("--gradient_steps", type=int, default=1)
-parser.add_argument("--train_freq", type=int, default=1)
-parser.add_argument("--env", type=str, default=options["ENV"],
-                    help="OpenAI Gym environment to perform algorithm on.")
+parser.add_argument("--agent-count", type=int, help="Number of agents.",
+                    default=options["AGENT_COUNT"])
 parser.add_argument("--device", type=str, default=options["DEVICE"],
                     choices=["cpu", "cuda", "auto"],
                     help="Device to use, either 'cpu', 'cuda' for GPU or "
                          "'auto'.")
-parser.add_argument("--load", type=bool, default=options["LOAD"])
+parser.add_argument("--env", type=str, default=options["ENV"],
+                    help="OpenAI Gym environment to perform algorithm on.")
 parser.add_argument("--seed", type=int, default=1,
                     help="Random seed in [0, 2 ** 32)")
-parser.add_argument("--buffer-start-size", type=int,
-                    default=options["BUFFER_START_SIZE"],
-                    help="Minimum replay buffer size before performing "
-                         "gradient updates.")
-parser.add_argument("--batch-size", type=int, default=options["BATCH_SIZE"],
-                    help="Minibatch size")
-parser.add_argument("--min-epoch-length", type=int,
-                    default=options["MIN_EPOCH_LEN"],
-                    help="Minimal length of a training epoch.")
-parser.add_argument("--wandb", type=str, default=options["WANDB"])
+# Training
+training = parser.add_argument_group("Training")
+training.add_argument("--steps", type=int, default=options["STEPS"],
+                      help="Total number of time steps to train the agent.")
+training.add_argument("--eval-interval", type=int,
+                      default=options["EVAL_INTERVAL"],
+                      help="Interval in time steps between evaluations.")
+training.add_argument("--n-eval-episodes", type=int,
+                      default=options["EVAL_N_RUNS"],
+                      help="Number of episodes for each evaluation.")
+training.add_argument("--buffer-size", type=int,
+                      default=options["BUFFER_SIZE"])
+training.add_argument("--buffer-start-size", type=int,
+                      default=options["BUFFER_START_SIZE"],
+                      help="Minimum replay buffer size before performing "
+                           "gradient updates.")
+training.add_argument("--batch-size", type=int, default=options["BATCH_SIZE"],
+                      help="Minibatch size")
+training.add_argument("--min-epoch-length", type=int,
+                      default=options["MIN_EPOCH_LEN"],
+                      help="Minimal length of a training epoch.")
+training.add_argument("--learning_rate", type=float,
+                      default=options["LEARNING_RATE"])
+training.add_argument("--tau", type=float, default=options["TAU"])
+training.add_argument("--gamma", type=float, default=options["GAMMA"])
+training.add_argument("--gradient_steps", type=int,
+                      default=options["GRAD_STEPS"])
+training.add_argument("--train_freq", type=int, default=options["TRAIN_FREQ"])
+# Agents
+agent_parser = parser.add_argument_group("Agent")
+agent_parser.add_argument("--mix-agents", type=bool,
+                          default=options["MIX_AGENTS"])
+agent_parser.add_argument("--net-arch", type=list,
+                          default=options["NET_ARCH"])
+# Peer Learning
+peer_learning = parser.add_argument_group("Peer Learning")
+peer_learning.add_argument("--follow-steps", type=int,
+                           default=options["FOLLOW_STEPS"])
+peer_learning.add_argument("--switch-ratio", type=float,
+                           default=options["SWITCH_RATIO"],
+                           help="Ratio of peer learning episodes to solo"
+                                "episodes; 0 -> only peer learning episodes.")
+peer_learning.add_argument("--peer-learning", type=bool,
+                           default=options["PEER_LEARNING"])
+peer_learning.add_argument("--use-agent-value", type=bool,
+                           default=options["USE_AGENT_VALUE"])
+peer_learning.add_argument("--use-trust", type=bool,
+                           default=options["USE_TRUST"])
+peer_learning.add_argument("--use-trust-buffer", type=bool,
+                           default=options["USE_TRUST_BUFFER"])
+peer_learning.add_argument("--use-critic", type=bool,
+                           default=options["USE_CRITIC"])
+peer_learning.add_argument("--trust-lr", type=float,
+                           default=options["TRUST_LR"])
+peer_learning.add_argument("--T", type=float, default=options["T"])
+peer_learning.add_argument("--T-decay", type=float, default=options["T_DECAY"])
+# WANDB
+parser.add_argument("--wandb", type=str, default=options["WANDB"],
+                    choices=["online", "offline", "disabled"])
 
 # parse args
 args = parser.parse_args()
@@ -113,8 +137,9 @@ option_on = (args.use_trust or args.use_critic or args.use_agent_value)
 assert (option_on and args.peer_learning) or not option_on
 
 # create results/experiments folder
+unique_dir = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 experiment_folder = Path.cwd().joinpath("Experiments", args.save_name,
-                                        wandb.util.generate_id())
+                                        unique_dir)
 experiment_folder.mkdir(exist_ok=True, parents=True)
 
 # init wandb
@@ -132,7 +157,7 @@ def make_env(seed=0):
     env = gym.make(args.env)
     env.seed(seed)
     env = Monitor(env)
-    return env
+    return DummyVecEnv([lambda: env])
 
 
 # initialize peer group
@@ -140,9 +165,11 @@ algo_args = dict(policy="MlpPolicy", verbose=1,
                  policy_kwargs=dict(log_std_init=-3, net_arch=args.net_arch),
                  buffer_size=args.buffer_size,
                  batch_size=args.batch_size,
-                 ent_coef='auto', gamma=args.gamma, tau=args.tau, train_freq=args.train_freq,
+                 ent_coef='auto', gamma=args.gamma, tau=args.tau,
+                 train_freq=args.train_freq,
+                 gradient_steps=args.gradient_steps,
                  learning_starts=args.buffer_start_size, use_sde=True,
-                 learning_rate=args.learning_rate, gradient_steps=args.gradient_steps,
+                 learning_rate=args.learning_rate,
                  tensorboard_log=str(experiment_folder),
                  device=args.device)
 
