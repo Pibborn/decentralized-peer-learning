4c8bde69bb31cf86e484e5ffae1642fc17a71ec4
jbrugger <wwjbrugger@gmail.com>
2022-05-12 11:06:43 +0200

    update scripts for Critic|Agent value| Trust

--- scripts_critic_agent_trust/run_peer_4_agent_C_HalfCheetahBulletEnv-v0.sh
+++ scripts_critic_agent_trust/run_peer_4_agent_C_HalfCheetahBulletEnv-v0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=09_05_22_peer_4_agent_HalfCheetahBulletEnv-v0      # Job name
+#SBATCH --job-name=11_05_22_peer_4_agent_HalfCheetahBulletEnv-v0      # Job name
 #SBATCH -p smp                 # The partition your job should run #in. devel smp parallel
 #SBATCH --account=m2_datamining   # Specify allocation to charge against m$
 #SBATCH --time=96:00:00          # Run time (hh_mm_ss) - 30 seconds
@@ -11,8 +11,8 @@
 
 #SBATCH -o \%x_\%j_profile.out # Specify stdout output file where \%j e$
 #SBATCH -C anyarch
-#SBATCH -o output_09_05_22/\%x_\%j.out
-#SBATCH -e output_09_05_22/\%x_\%j.err
+#SBATCH -o output_11_05_22/\%x_\%j.out
+#SBATCH -e output_11_05_22/\%x_\%j.err
 
 #SBATCH --mail-user=jbrugge@students.uni-mainz.de
 #SBATCH --mail-type=FAIL
@@ -26,7 +26,7 @@ export http_proxy=http://webproxy.zdv.uni-mainz.de:8888
 export https_proxy=https://webproxy.zdv.uni-mainz.de:8888
 source virt/bin/activate
 wandb offline
-srun python run_peer.py --save-name peer_4_agent_C_HalfCheetahBulletEnv-v0_09_05_22 \
-  --job_id %j --env HalfCheetahBulletEnv-v0 --agent-count 4 --batch-size 256 --buffer-size 300_000 \
-  --steps 1_000_000 --buffer-start-size 10_000 --learning_rate 7.3e-4 --gamma 0.98 --gradient_steps 8 --tau 0.02 --train_freq 8
-  --seed 0 --use-critic True --use-agent-value False --use-trust False --peers-sample-with-noise False
+srun python run_peer.py --save-name peer_4_agent_C_HalfCheetahBulletEnv-v0_11_05_22 \
+  --job_id $SLURM_JOB_ID --env HalfCheetahBulletEnv-v0 --agent-count 4 --batch-size 256 --buffer-size 300_000 \
+  --steps 1_000_000 --buffer-start-size 10_000 --learning_rate 7.3e-4 --gamma 0.98 --gradient_steps 8 --tau 0.02 --train_freq 8 \
+  --seed $SLURM_JOB_ID --use-critic True --use-agent-value False --use-trust False --peers-sample-with-noise False
