589280c4d4afee624346518fbb40dc33daaec469
vanillawhey <cedric.derstroff@gmx.net>
2022-05-13 12:52:57 +0200

    Introducing the weighted dictator

--- new_dictator.py
+++ new_dictator.py
@@ -12,19 +12,22 @@ def make_dictator_class(cls: Type[OffPolicyAlgorithm]):
     :param cls: The learning algorithm (needs to have a callable critic)
     :return: The mixed in dictator agent class
     """
+
     class Dictator(make_peer_class(cls)):
-        def __init__(self, temperature, temp_decay, algo_args, env_func,
-                     sample_actions=True, greedy_suggestions=True, seed=None):
-            super(Dictator, self).__init__(temperature=temperature,
-                                           temp_decay=temp_decay,
-                                           algo_args=algo_args,
-                                           solo_training=False,
-                                           env_func=env_func,
-                                           use_trust=True,
-                                           follow_steps=1,
-                                           seed=seed)
+        def __init__(self, temperature, temp_decay, algo_args, env,
+                     sample_actions=True, peers_sample_with_noise=False,
+                     seed=None):
+            super(Dictator, self).__init__(
+                peers_sample_with_noise=peers_sample_with_noise,
+                temperature=temperature,
+                temp_decay=temp_decay,
+                algo_args=algo_args,
+                solo_training=False,
+                use_critic=True,
+                follow_steps=1,
+                seed=seed,
+                env=env)
             self.sample_actions = sample_actions
-            self.greedy_suggestions = greedy_suggestions
 
         def predict(self, observation, deterministic=False, **_):
             """ The dictator always involves every party in the decision-
@@ -52,3 +55,44 @@ def make_dictator_class(cls: Type[OffPolicyAlgorithm]):
             return OffPolicyAlgorithm.learn(self, **kwargs)
 
     return Dictator
+
+
+def make_weighted_dictator_class(cls: Type[OffPolicyAlgorithm]):
+    """ Creates a mixin with the corresponding algorithm class
+    :param cls: The learning algorithm (needs to have a callable critic)
+    :return: The mixed in weighted dictator agent class
+    """
+
+    class WeightedDictator(make_dictator_class(cls)):
+        def __init__(self, temperature, temp_decay, algo_args, env,
+                     sample_actions=True, peers_sample_with_noise=False,
+                     seed=None):
+            super(WeightedDictator, self).__init__(temperature, temp_decay,
+                                                   algo_args, env,
+                                                   sample_actions,
+                                                   peers_sample_with_noise,
+                                                   seed)
+            self.eval_callback = None
+
+        def critique(self, observations, actions) -> np.array:
+            values = np.empty(self.n_peers)
+            for i, peer in enumerate(self.group.peers):
+                if peer.eval_callback is None or \
+                  len(peer.eval_callback.evaluations_results) == 0:
+                    # before the first evaluation treat everyone the same
+                    return np.ones(self.n_peers)
+                values[i] = np.mean(peer.eval_callback.evaluations_results[-1])
+            return values
+
+        def learn(self, _, **kwargs):
+            # store callback
+            self.eval_callback = kwargs["callback"][0]
+
+            super(WeightedDictator, self).learn(_, **kwargs)
+
+        def _excluded_save_params(self):
+            ex_list = super()._excluded_save_params()  # noqa
+            ex_list.extend(["eval_callback"])
+            return ex_list
+
+    return WeightedDictator
